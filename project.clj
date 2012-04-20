fba49b67debcf4a36eae9dddb48c16f8adb4c455
ryan spangler <ryan.spangler@gmail.com>
2012-04-20 15:04:00 -0700

    versions

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject antler/caribou-frontend "0.2.5"
+(defproject antler/caribou-frontend "0.3.0"
   :description "The page routing ring handler for caribou"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [antler/caribou-core "0.5.0"]
