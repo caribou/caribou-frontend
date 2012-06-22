4107977b83ed259f6bbda3d699dc9b6ff920e2e8
Matthew Willhite <matt.willhite@weareinstrument.com>
2012-06-22 10:54:22 -0700

    Bumped the version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject antler/caribou-frontend "0.5.2"
+(defproject antler/caribou-frontend "0.5.3"
   :description "The page routing ring handler for caribou"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [antler/caribou-core "0.6.5"]
