9478a8c7ed4884c98ce7768b615a2456ec184bff
Justin Glenn Smith <noisesmith@gmail.com>
2012-07-24 11:09:10 -0700

    bumping for new feature merge

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject antler/caribou-frontend "0.5.6"
+(defproject antler/caribou-frontend "0.5.7"
   :description "The page routing ring handler for caribou"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [antler/caribou-core "0.6.7"]
