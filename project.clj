cdae6e596a44d674940adf790ea31a6f3310365c
ryan spangler <ryan.spangler@gmail.com>
2012-08-03 15:03:42 -0700

    added newest stencil version

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject antler/caribou-frontend "0.5.10"
+(defproject antler/caribou-frontend "0.5.11"
   :description "The page routing ring handler for caribou"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [antler/caribou-core "0.6.10"]
                  [compojure "1.0.4"]
                  [clj-time "0.3.6"]
-                 [antler/stencil "0.3.1"]
+                 [antler/stencil "0.3.2"]
                  [fuziontech/ring-json-params "0.2.0"]
                  [ring "1.1.0"]
                  [org.clojars.doo/cheshire "2.2.3"]]
