6ce3c76fb3c73d9b0cf0b32ae58785270efcb9a9
ryan spangler <ryan.spangler@gmail.com>
2012-10-05 15:31:30 -0700

    latest localization

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject antler/caribou-frontend "0.5.23"
+(defproject antler/caribou-frontend "0.7.0"
   :description "The page routing ring handler for caribou"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [antler/caribou-core "0.6.19"]
+                 [antler/caribou-core "0.7.0"]
                  [compojure "1.0.4"]
                  [clj-time "0.3.6"]
                  [antler/stencil "0.3.5"]
