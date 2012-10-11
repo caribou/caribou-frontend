6af05b23f241b540e90a299ced7a4d6edcb35323
ryan spangler <ryan.spangler@gmail.com>
2012-10-11 10:32:10 -0700

    deps

--- project.clj
+++ project.clj
@@ -2,11 +2,14 @@
   :description "The page routing ring handler for caribou"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [antler/caribou-core "0.7.2"]
-                 [compojure "1.0.4"]
+                 [compojure "1.1.3"]
                  [clj-time "0.3.6"]
                  [antler/stencil "0.3.5"]
                  [fuziontech/ring-json-params "0.2.0"]
-                 [ring "1.1.0"]
+                 [ring "1.1.6"
+                  :exclusions [org.clojure/clojure
+                               clj-stacktrace
+                               hiccup]]
                  [org.clojars.doo/cheshire "2.2.3"]]
   :jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n"]
   :autodoc {:name "Caribou Frontend"
