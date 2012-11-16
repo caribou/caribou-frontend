fce15683c256987bfa35fa83cfd6a936826ddcb8
Ryan Roemmich <ralfonso@gmail.com>
2012-11-16 11:28:08 -0700

    bump clj-time version

--- project.clj
+++ project.clj
@@ -2,8 +2,8 @@
   :description "The page routing ring handler for caribou"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [antler/caribou-core "0.7.12"]
+                 [clj-time "0.4.4"]
                  [compojure "1.1.3"]
-                 [clj-time "0.3.6"]
                  [antler/stencil "0.3.5"]
                  [fuziontech/ring-json-params "0.2.0"]
                  [ring "1.1.6"
