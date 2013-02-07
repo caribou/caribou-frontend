74267150fac845a2bd927e69c3a3469ff40656f6
Kyle Dawkins <kyle.dawkins@weareinstrument.com>
2013-02-06 17:26:44 -0800

    Version bump to match core

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject antler/caribou-frontend "0.8.0"
+(defproject antler/caribou-frontend "0.8.2"
   :description "The page routing ring handler for caribou"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [antler/caribou-core "0.8.0"]
+                 [antler/caribou-core "0.8.2"]
                  [clj-time "0.4.4"]
                  [compojure "1.1.3" :exclusions [ring/ring-core ring]]
                  [antler/stencil "0.4.5"]
