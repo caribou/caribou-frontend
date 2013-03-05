d185b61c91070b1b86e1877c9bf63a27841de9ea
Ryan Roemmich <ralfonso@gmail.com>
2013-03-05 14:49:13 -0700

    ver

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject antler/caribou-frontend "0.9.6"
+(defproject antler/caribou-frontend "0.9.7"
   :description "The page routing ring handler for caribou"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [antler/caribou-core "0.9.1"]
