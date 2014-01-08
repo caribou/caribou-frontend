1d236a68adefa337fdbb17eebf7c0844bb64539c
Justin Smith <noisesmith@gmail.com>
2014-01-08 15:32:15 -0800

    keeping in synch with core develop branch

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject caribou/caribou-frontend "0.13.3"
   :description "The page routing ring handler for caribou"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [caribou/caribou-core "0.13.1"]
+                 [caribou/caribou-core "0.13.3"]
                  [cljsbuild "0.3.3"
                   :exclusions [fs]]
                  [clj-time "0.4.4"]
